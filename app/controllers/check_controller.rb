35d0b764f112b17f52eefb71026d4e18435ed19f
Frederic Rocher <fredericrocher@macbook-pro-de-frederic.home>
2015-09-27 22:42:00 +0200

    tests : 1st version

--- app/controllers/check_controller.rb
+++ app/controllers/check_controller.rb
@@ -1,15 +1,17 @@
+require 'tempfile'
+
 class CheckController < ApplicationController
   def index
     url = params[:url]
-    file = File.join(Rails.root, 'app', 'phantom', 'check.js')
-    cmd = 'phantomjs --ssl-protocol=any ' + file + ' ' + url
+    # TODO : sanitize url
+    jsonFile = Tempfile.new('twb')
+    harFile = Tempfile.new('twb')
+    cmd = "browsertime -u #{url} -n 1 --filename #{jsonFile.path} --harFile #{harFile.path}"
     stdout,stderr,status = Open3.capture3(cmd)
     if status.success?
-      Rails.logger.info "*********** success for " + url
-      Rails.logger.info stdout
-      render json: stdout
+      render json: jsonFile.read
     else
-      render json: { error: "error"}, status: 422
+      render json: { status: "error"}, status: 422
       Rails.logger.error stdout
       Rails.logger.error stderr
     end
--- app/controllers/uptime_controller.rb
+++ app/controllers/uptime_controller.rb
@@ -1,4 +1,23 @@
+require "uri"
 class UptimeController < ApplicationController
   def index
+    url = params[:url]
+    url = URI.escape(url)
+    # TODO : sanitize url
+    url = ActionController::Base.helpers.sanitize(url)
+    cmd = "curl -L #{url}"
+    stdout,stderr,status = Open3.capture3(cmd)
+    if status.success?
+      if stdout.include?("Could not resolve")
+        render json: { status: "failed"}, status: 200
+      else
+        render json: { status: "success"}, status: 200
+      end
+    else
+      render json: { status: "error"}, status: 422
+      Rails.logger.error stdout
+      Rails.logger.error stderr
+    end
+
   end
 end
