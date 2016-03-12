0710ccd4d663a048e70dc83fb755be76ab9caa21
Frederic Rocher <fredericrocher@macbook-pro-de-frederic.home>
2016-03-12 23:36:55 +0100

    Add new parameter to check command

--- app/controllers/check_controller.rb
+++ app/controllers/check_controller.rb
@@ -3,11 +3,23 @@ require 'tempfile'
 class CheckController < ApplicationController
   def index
     url = sanitized_url
+    target = params[:target]
+
+    connection = ""
+    size = ""
+    if target == "mobile"
+      connection = "mobilefast3g"
+      size = "357x627"
+    else
+      connection = "cable"
+      size = "1280x1024"
+    end
 
     jsonFile = Tempfile.new('bnb')
     harFile = Tempfile.new('bnb')
     browser = Rails.configuration.browser
-    cmd = "browsertime -u #{url} -b #{browser} -n 1 --filename #{jsonFile.path} --harFile #{harFile.path}"
+    cmd = "browsertime -u #{url} -b #{browser} -w #{size} --connection #{connection} -n 1 --filename #{jsonFile.path} --harFile #{harFile.path}"
+    Rails.logger.info cmd
     stdout,stderr,status = Open3.capture3(cmd)
     if status.success?
       render json: {stats: JSON.parse(jsonFile.read), har: JSON.parse(harFile.read)}
