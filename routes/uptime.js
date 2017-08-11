e01896fd99fe8878f5d03fcdfd581142fb446ef2
Fred Rocher <frocher2005@gmail.com>
2017-08-11 18:47:57 +0200

    Add no-cache to requests

--- routes/uptime.js
+++ routes/uptime.js
@@ -10,45 +10,48 @@ function checkKeyword(data, keyword, checkType) {
 
 function doubleCheck() {
   return new Promise( (resolve, reject) => {
-    request.get('https://www.google.com').end(function(err, res) {
-      resolve(res.status < 300);
-    });
+    request.get('https://www.google.com')
+      .set('Cache-Control', 'no-cache,no-store,must-revalidate,max-age=-1,private')
+      .end(function(err, res) {
+        resolve(res.status < 300);
+      });
   });
 }
 
 router.get('/', function(req, res, next) {
-  request.get(req.query.url).end(function(err, result) {
-
-    if (!result || result.status >= 300) {
-      doubleCheck().then(isCheck => {
-        if (isCheck) {
-          if (result) {
-            res.send({status: 'failed', errorMessage: `Status code failed : ${result.status}`, content: result.text});
+  request.get(req.query.url)
+    .set('Cache-Control', 'no-cache,no-store,must-revalidate,max-age=-1,private')
+    .end(function(err, result) {
+      if (!result || result.status >= 300) {
+        doubleCheck().then(isCheck => {
+          if (isCheck) {
+            if (result) {
+              res.send({status: 'failed', errorMessage: `Status code failed : ${result.status}`, content: result.text});
+            }
+            else {
+              res.send({status: 'failed', errorMessage: `Can't check ${req.query.url}. Code: ${err.code}`});
+            }
           }
           else {
-            res.send({status: 'failed', errorMessage: `Can't check ${req.query.url}. Code: ${err.code}`});
+            res.send({status: "success"});
           }
-        }
-        else {
-          res.send({status: "success"});
-        }
-      });
-    }
-    else {
-      if (!req.query.keyword) {
-        res.send({status: "success"});
+        });
       }
       else {
-        let checkType = req.query.type || "presence";
-        if (checkKeyword(result.text, req.query.keyword, checkType)) {
+        if (!req.query.keyword) {
           res.send({status: "success"});
         }
         else {
-          res.send({status: 'failed', errorMessage: `Check of ${checkType} of ${req.query.keyword} failed.`, content: result.text});
+          let checkType = req.query.type || "presence";
+          if (checkKeyword(result.text, req.query.keyword, checkType)) {
+            res.send({status: "success"});
+          }
+          else {
+            res.send({status: 'failed', errorMessage: `Check of ${checkType} of ${req.query.keyword} failed.`, content: result.text});
+          }
         }
       }
-    }
-  });
+    });
 });
 
 module.exports = router;
